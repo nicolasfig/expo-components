71b644d32650ed0192739ccc3524828832da00a3
Nicolas Figueroa <nicolasfigb@gmail.com>
2019-07-17 22:54:18 -0500

    ImagePicker and permissions

--- Camera.js
+++ Camera.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import {Image, Text, View, StyleSheet} from 'react-native';
+import {Button, Image, Text, View, StyleSheet} from 'react-native';
 import Constants from 'expo-constants';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 export default class Camera extends Component {
+
+    _launchCameraAsync = async () => {
+        let {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if(status !== 'granted'){
+            console.error("Camera roll perms not granted");
+            return;
+        } 
+
+        let image = await ImagePicker.launchImageLibraryAsync();
+        console.log(image);
+    }
+
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Hello Camera</Text>
-                <Image source={require('./assets/whale.jpeg')} style={styles.image}/>
+                <View style={styles.imageContainer}>
+                    <Image source={require('./assets/whale.jpeg')} style={styles.image}/>
+                    <Image source={require('./assets/whale2.jpeg')} style={styles.image}/>
+                </View>
+                <Button onPress={this._launchCameraAsync} title="Gallery"/>
             </View>
         )
     }
@@ -18,11 +36,14 @@ const styles = {
         flex: 1,
 		backgroundColor: "#fff",
 		alignItems: "center",
-		justifyContent: "center",
-		paddingTop: Constants.statusBarHeight
+        justifyContent: "center",
+		marginTop: Constants.statusBarHeight
+    },
+    imageContainer: {
+        flexDirection: "row",
     },
     image: {
-        Width: 300,
-        Height: 400,
+        width: 200,
+        height: 200,
     }
 }
